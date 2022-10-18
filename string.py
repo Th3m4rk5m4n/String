b13f43f136b431e7fbca12eb4e90f7d7c1ce9814
5kipp3r <112339635+jayvin1244@users.noreply.github.com>
2022-10-18 18:00:41 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,111 @@
+"""
+mystr = "python "
+print(mystr)
+"""
+
+
+
+
+# this is string slicing 
+
+mystr = "jayvin"
+print(mystr[:2:4])
+
+
+# this is chack length of string
+"""
+mystr = "jayvin is good boy"
+print(len(mystr))
+"""
+
+
+"""
+mystr = "jayvin is good boy"
+print(len(mystr))
+print(mystr[0:5:2])
+"""
+
+"""
+mystr = "jayvin is good boy"
+print(len(mystr))
+print(mystr[:6])
+"""
+
+"""
+mystr = "jayvin is good boy"
+print(len(mystr))
+print(mystr[::])     # this is use all by defoult ::
+"""
+
+"""
+#  -5 is chack length in revierce
+# [::-5]
+# [::-1]   etc..........
+like  
+1 2 3 4 5 6 
+j a y v i n  
+mystr = "jayvin is good boy"
+print(len(mystr))
+print(mystr[-5:])
+"""
+
+
+"""
+
+mystr = "jayvin is good boy"
+print(mystr.isalnum())          # ans is true and false 
+                                # boolean variable
+# that is alpha numeric that why ans is "false"
+# we remove the space then this ans is "true" 
+ """
+
+
+# that is chack alpha numeric
+"""
+mystr = "jayvin is good boy"
+print(mystr.isalpha())
+"""
+
+
+"""
+# this is chack for the last statement end of the statement
+mystr = "jayvin is good boy" 
+print(mystr.endswith("boy"))    # ans is true that statement is endswith the "boy "
+"""
+
+"""
+# this count is use to count the one charecter like "o"
+mystr = "jayvin is good boy"
+print(mystr.count("o"))             #the ans is 3  
+"""
+
+
+
+# capitalise
+"""
+mystr = "jayvin is good boy"
+print(mystr.capitalize()) 
+"""
+
+#find
+"""
+mystr = "jayvin is good boy"
+print(mystr.find("good"))
+"""
+
+
+"""
+# lowercase \ upper case
+mystr = "jayvin is good boy"
+print(mystr.lower())                # that is connvert case upper to lower and lower to upper
+print(mystr.upper())
+
+"""
+
+
+"""
+# replace
+mystr = "jayvin is good boy"      # replase the value 
+print(mystr.replace("is" , "are "))
+"""
+
